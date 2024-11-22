68af800b9fcf952c72eac273e25dfe9c404a9da2
Emilio <emifont01@gmail.com>
2024-11-22 09:35:30 -0500

    feat: update lsp config

--- lua/kickstart/plugins/lsp-config.lua
+++ lua/kickstart/plugins/lsp-config.lua
@@ -178,6 +178,7 @@ return {
           },
           gofumpt = true,
         },
+        jsonls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
