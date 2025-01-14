f7964802cb5c4223c26c5e9039b76d64223c1de3
Emilio <emifont01@gmail.com>
2025-01-14 16:59:10 -0500

    feat: adds flash

--- lua/lazy-plugin.lua
+++ lua/lazy-plugin.lua
@@ -77,12 +77,13 @@ require('lazy').setup({
   require 'kickstart.plugins.alpha',
   require 'kickstart.plugins.hardtime',
   require 'kickstart.plugins.auto-session',
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
-  --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  { import = 'custom.plugins' },
+  require 'kickstart.plugins.flash',
+    -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+    --    This is the easiest way to modularize your config.
+    --
+    --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+    --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
+    { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
